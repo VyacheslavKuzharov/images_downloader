f5394de5ee80ff709537af51b5a18cb314f62ef6
Vyacheslav Kuzharov <vyacheslav.kuzharov@gmail.com>
2021-06-02 10:18:15 +0300

    Initial commit

---
+++ lib/images_downloader.rb
@@ -0,0 +1,6 @@
+require "images_downloader/version"
+
+module ImagesDownloader
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/images_downloader/version.rb
@@ -0,0 +1,3 @@
+module ImagesDownloader
+  VERSION = "0.1.0"
+end
